9d026fb5c57c356e3c308cc969ba211d162fb278
BeauChewning <beauchewning96@gmail.com>
2021-09-30 17:11:41 -0400

    final changes

--- Assets/assets/script.js
+++ Assets/assets/script.js
@@ -14,15 +14,18 @@ $(document).ready(function () {
     $('.saveBtn').on('click', function () {
         console.log("test")
         var value = $(this).siblings('.description').val();
-        var time = $('this').parent().attr('id');
+        var time = $(this).parent().attr('id');
         console.log(value);
         console.log(time)
+        localStorage.setItem(time, value)
 
     })
     var currentHour = moment().hours();
     console.log(currentHour);
 
 })
+
+
 
 var timeOfday = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
 updatetime();
@@ -46,3 +49,22 @@ function updatetime() {
     }
   }
 }
+
+
+
+
+$("#hour-9 .description").val(localStorage.getItem('hour-9'));
+$("#hour-10 .description").val(localStorage.getItem('hour-10'));
+$("#hour-11 .description").val(localStorage.getItem('hour-11'));
+$("#hour-12 .description").val(localStorage.getItem('hour-12'));
+$("#hour-13 .description").val(localStorage.getItem('hour-13'));
+$("#hour-14 .description").val(localStorage.getItem('hour-14'));
+$("#hour-15 .description").val(localStorage.getItem('hour-15'));
+$("#hour-16 .description").val(localStorage.getItem('hour-16'));
+$("#hour-17 .description").val(localStorage.getItem('hour-17'));
+//for(var i = 9; i > 17; i++){}
+  
+
+
+
+
